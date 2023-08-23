3543382ef3e0461a95e0bb19db50f1ab17e1efbf
Rami-Youssef <ramiyoussefxd@gmail.com>
2023-08-23 11:55:32 +0100

    footer with pototype logos

---
+++ vue/footer/footer.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        footer {
+        background-color: #0a0c10;
+        color: #fff;
+        padding: 60px 0;
+        }
+        footer h2{
+        color:white; 
+        text-align: center;
+        background: rgb(175, 19, 19);
+        }
+        .footer-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        align-items: center;
+        max-width: 1200px;
+        margin: 0 auto;
+        }
+
+        .footer-logo img {
+        height: 100%;
+        width: auto;
+        }
+        .footer-contact h3 {
+        color: rgb(175, 19, 19) ;
+        margin-bottom: 20px;
+        }
+
+        .footer-contact p {
+        margin: 0 10px 10px 0;
+        font-size: 16px;
+        }
+        .footer-contact p img{
+            width: 50px;
+        }
+        .footer-container h4{
+            color:rgb(190, 19, 19) ;
+        }
+        .footer-container hr{
+            border:1px solid rgb(175, 19, 19) ;
+            height: 50px;
+        }
+    </style>
+</head>
+<body>
+    <footer>
+        <div>
+            <h2>INFORMATION</h2>
+       
+        <div class="footer-container">
+            <div class="footer-logo">
+            <img src="lesieur.png" alt="Logo">
+            </div>
+            <div class="footer-contact">
+            <h3>Contactez-nous :</h3>
+            <p>Rue Caporal Corbi</p>
+            <p>Maroc, Casablanca</p>
+            <p>Phone: (xxx) xxx-xxxx</p>
+            <p>Email: xxxx@Gmail.com</p>
+            </div>
+            <div class="footer-contact">
+            <h3>Suivez-nous :</h3>
+            <p> <img src="facebook.png" alt=""></p>
+            <p> <img src="youtube.png" alt=""></p>
+            <p> <img src="instagram.png" alt=""></p>
+            <p></p>
+            </div>
+            <div class="footer-logo">
+            <img src="cristal.png" alt="Logo">
+            </div>
+        </div>
+    </footer>
+
+</html>
