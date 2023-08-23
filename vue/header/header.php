8074a969de74131da2975644243a080251c4630e
Rami-Youssef <ramiyoussefxd@gmail.com>
2023-08-23 09:29:11 +0100

    first files

---
+++ vue/header/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #header{
+            background-color:  ;
+            display: flex;
+        }
+        #logo img{
+            width: 500px;
+        }
+        
+    </style>
+</head>
+<body>
+    <header id="header">
+        <div>
+            <a href="">Home</a>
+        </div>
+        <div id="logo"><img src="logo.png" alt=""></div>
+        <div>
+            <p><a href="">Log-in</a></p>
+            <p>/</p>
+            <p><a href="">sign-in</a></p>
+        </div>
+        <div>
+            <p>how</p>
+        </div>
+    </header>
+</body>
+</html>
